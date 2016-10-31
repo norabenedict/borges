a73cf1a1aff74be716df0a1a6760f0c9a7d7e90e
Jeremy Boggs <jeremyboggs@gmail.com>
2016-10-31 15:52:28 -0400

    First pass at working map with location data.

---
+++ locations.js
@@ -0,0 +1,102 @@
+var locations = [
+{
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [[-58.43062439999999,-34.6065302], [-58.431854999999985,-34.6068083]]
+
+  },
+  "properties": {
+    "name": "Editorial Tor"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-58.380773299999994,-34.6076913]
+  },
+  "properties": {
+    "name": "Editorial Tor: Salón de exposiciones y ventas"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [[-58.395323899999994,-34.6117177], [-58.373913200000004,-34.6030088]]
+  },
+  "properties": {
+    "name": "Emecé Editores",
+    "category": "Publisher"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-58.44458829999997,-34.6289292]
+  },
+  "properties": {
+    "name": "Los talleres gráficos \"Colón\"",
+    "category": "Printer"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [[-58.48425780000002,-34.5755365], [-58.41141679999999,-34.5859937], [-58.3958606,-34.59579]]
+  },
+  "properties": {
+    "name": "Manuel Gleizer",
+    "category": "Publisher"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [[-58.3745515,-34.6156579], [-58.3745212,-34.6156325]]
+  },
+  "properties": {
+    "name": "Imprenta López",
+    "category": "Printer"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-58.38251200000002,-34.6032326]
+  },
+  "properties": {
+    "name": "Los talleres gráficos J. Hays Bells",
+    "category": "Printer"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [[-58.373696499999994,-34.6207096], [-58.37352099999999,-34.6102964], [-58.38392750000003,-34.6144447]]
+  },
+  "properties": {
+    "name": "Editorial Sudamericana",
+    "category": "Publisher"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "MultiPoint",
+    "coordinates": [[-58.38870250000003,-34.6041913], [-58.38235680000003,-34.6108857], [-58.37907899999999,-34.6139104]]
+  },
+  "properties": {
+    "name": "Editorial Losada",
+    "category": "Publisher"
+  }
+},{
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-58.439775499999996,-34.601597],
+  },
+  "properties": {
+    "name": "Macagno, Landa y Cía",
+    "category": "Publisher"
+    }
+  }
+];
