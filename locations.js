5d3189fea063aeade1e23f3908d9e7bf2a9594c9
Nora Benedict <ncb3ka@virginia.edu>
2016-11-14 15:09:02 -0500

    changes

--- locations.js
+++ locations.js
@@ -8,7 +8,7 @@ var locations = [
   },
   "properties": {
     "name": "Editorial Tor",
-    "category": "Publisher & Printer / Editorial & Imprenta",
+    "category": "Publisher & Printer",
     "description": "",
     "images": ["Tor-advertisement2.jpg"]
 
@@ -18,10 +18,11 @@ var locations = [
   "geometry": {
     "type": "Point",
     "coordinates": [-58.380773299999994,-34.6076913]
+
   },
   "properties": {
     "name": "Editorial Tor: Salón de exposiciones y ventas",
-    "category": "Publisher & Printer / Editorial & Imprenta"
+    "category": "Publisher & Printer"
 
   }
 },{
@@ -29,10 +30,11 @@ var locations = [
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [[-58.395323899999994,-34.6117177], [-58.373913200000004,-34.6030088]]
+
   },
   "properties": {
     "name": "Emecé Editores",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
     "images":["Emecé-editores-dirección.jpg"]
 
@@ -42,10 +44,11 @@ var locations = [
   "geometry": {
     "type": "Point",
     "coordinates": [-58.44458829999997,-34.6289292]
+
   },
   "properties": {
     "name": "Los talleres gráficos \"Colón\"",
-    "category": "Printer / Imprenta",
+    "category": "Printer",
     "description": "",
     "images": ["Colombo-honorary-dinner.jpg"]
 
@@ -55,10 +58,11 @@ var locations = [
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [[-58.48425780000002,-34.5755365], [-58.41141679999999,-34.5859937], [-58.3958606,-34.59579]]
+
   },
   "properties": {
     "name": "Manuel Gleizer",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
     "images": [""]
 
@@ -68,144 +72,167 @@ var locations = [
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [[-58.3745515,-34.6156579], [-58.3745212,-34.6156325]]
+
   },
   "properties": {
     "name": "Imprenta López",
-    "category": "Printer / Imprenta",
+    "category": "Printer",
     "description": "",
     "images": ["Imprenta-lópez-shop2.jpg"]
+
   }
 },{
   "type": "Feature",
   "geometry": {
     "type": "Point",
     "coordinates": [-58.38251200000002,-34.6032326]
+
   },
   "properties": {
     "name": "Los talleres gráficos J. Hays Bells",
-    "category": "Printer / Imprenta",
+    "category": "Printer",
     "description": "",
-    "images": [""]
+    "images": []
+
   }
 },{
   "type": "Feature",
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [[-58.373696499999994,-34.6207096], [-58.37352099999999,-34.6102964], [-58.38392750000003,-34.6144447]]
+
   },
   "properties": {
     "name": "Editorial Sudamericana",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
     "images": ["Editorial-sudamericana-shop.jpg"]
+
   }
 },{
   "type": "Feature",
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [[-58.38870250000003,-34.6041913], [-58.38235680000003,-34.6108857], [-58.37907899999999,-34.6139104]]
+
   },
   "properties": {
     "name": "Editorial Losada",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "bananas",
     "images": ["Editorial-losada-shop.jpg"]
+
   }
 },{
   "type": "Feature",
   "geometry": {
     "type": "Point",
-    "coordinates": [-58.439775499999996,-34.601597],
+    "coordinates": [-58.439775499999996,-34.601597]
+
   },
   "properties": {
     "name": "Macagno, Landa y Cía",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
-    "images": [""]
+    "images": []
+
 }
 }, {
   "type": "Feature",
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [[-58.37741490000002,-34.61742],[-58.374728,-34.5998378], [-58.37416359999998,-34.5999214]]
+
   },
   "properties": {
     "name": "Editorial Sur",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
     "images": ["Ocampo-exhibición.jpg"]
+
   }
 }, {
   "type": "Feature",
   "geometry": {
     "type": "Point",
-    "coordinates": [[-58.372112699999995,-34.6008154]]
+    "coordinates": [-58.372112699999995,-34.6008154]
+
   },
   "properties": {
     "name": "Américalee, Editora e Impresora",
-    "category": "Publisher & Printer / Editorial & Imprenta",
+    "category": "Publisher & Printer",
     "description": "",
-    "images": [""]
+    "images": []
+
   }
 }, {
   "type": "Feature",
   "geometry": {
     "type": "MultiPoint",
     "coordinates": [[-58.3845661999999955,-34.6038337], [-58.39007110000001,-34.603012]]
+
   },
   "properties": {
     "name": "Imprenta Iglesias y Matera",
-    "category": "Printer / Imprenta",
+    "category": "Printer",
     "description": "",
-    "images": [""]
+    "images": []
+
   }
 }, {
   "type": "Feature",
   "geometry": {
     "type": "Point",
-    "coordinates": [[-58.37538269999999,-34.6018597]]
+    "coordinates": [-58.37538269999999,-34.6018597]
+
   },
   "properties": {
     "name": "Domingo Viau y Zona",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
     "images": ["Viau-shop.jpg"]
+
   }
 }, {
   "type": "Feature",
   "geometry": {
     "type": "Point",
-    "coordinates": [[-58.37776889999998,-34.6097316]]
+    "coordinates": [-58.37776889999998,-34.6097316]
+
   },
   "properties": {
     "name": "Editorial Kapelusz y Cía",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
-    "images": [""]
+    "images": []
+
   }
 }, {
   "type": "Feature",
   "geometry": {
     "type": "Point",
-    "coordinates": [[-58.384394999999984,-34.607451]]
+    "coordinates": [-58.384394999999984,-34.607451]
+
   },
   "properties": {
     "name": "Talleres gráficos Linari y Cía",
-    "category": "Printer / Imprenta",
+    "category": "Printer",
     "description": "",
-    "images": [""]
+    "images": []
+
   }
 }, {
   "type": "Feature",
   "geometry": {
     "type": "Point",
-    "coordinates": [[-58.37472500000001,-34.615195]]
+    "coordinates": [-58.37472500000001,-34.615195]
+
   },
   "properties": {
     "name": "Editorial Nova",
-    "category": "Publisher / Editorial",
+    "category": "Publisher",
     "description": "",
-    "images": [""]
+    "images": []
   }
 }
 ];
